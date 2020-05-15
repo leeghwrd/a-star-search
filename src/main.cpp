5539398bb3939d6d402e58d612c8a2a1de051e89
leeghwrd <leeghwrd@gmail.com>
2020-05-15 00:28:30 -0700

    Add CMake build system
    - CMakeLists.txt at root of project
    
    - Move main.cpp to src/
    
    - Add build/ to .gitignore
    
    - Modify ReadBoardFile string relative path

--- src/main.cpp
+++ src/main.cpp
@@ -206,7 +206,7 @@ int main()
 {
   int init[2]{0, 0};
   int goal[2]{4, 5};
-  auto board = ReadBoardFile("boards/1.board");
+  auto board = ReadBoardFile("../boards/1.board");
   auto solution = Search(board, init, goal);
   PrintBoard(solution);
 }
